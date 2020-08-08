335c2b5855e1eddf307b2137a62ca9fd17243bdd
Benjamin Kindle <benjaminkindle@yahoo.com>
2020-08-08 08:48:53 -0400

    reproduce oauth library bug

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 This is an example project of an Angular workspace with a library and application.
 
 note: only building currently works
+
+## angular-oauth2-oidc error
+
+This branch shows an error that happens when the angular-oauth2-oidc library is used. The app builds fine, but when you serve with ibazel, there is an error. The error does not exist if a version of the library is packaged that doesn't include the ngfactory files.
--- projects/my-app/src/app/app.module.ts
+++ projects/my-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {MyLibModule} from '@literalpie/my-lib';
 
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import { OAuthModule } from "angular-oauth2-oidc";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     BrowserModule,
     MyLibModule,
+    OAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
